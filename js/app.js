ecdf35cf472feab82b0a473d5405b128a337a81f
Prateem Shrestha <prateems@gmail.com>
2014-12-10 01:03:43 -0500

    Removed save-related fluff. No implementation of that needed for now. Going to finish the app functionality before I start looking into letting saves happen.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ builder.controller("machine.main", ['$scope', function($scope) {
   $scope.sequence = "";
   $scope.result = "";
   $scope.runLog = [];
+  $scope.finalState = null;
 
   $scope.newState = {
     direction: 'right'
@@ -45,12 +46,10 @@ builder.controller("machine.main", ['$scope', function($scope) {
 
     for (var state of $scope.states) {
       if (state.name == "initial" && state.reads == initialChar) {
-        var finalState = state;
         $scope.result = $scope.sequence;
 
-        if (state.direction != "stop") {
-          finalState = __readSequence(0, state);
-        }
+        $scope.finalState = (state.direction != "stop")
+            ? __readSequence(0, state) : state;
 
         __machineRunCompleted = true;
         break;
@@ -66,18 +65,7 @@ builder.controller("machine.main", ['$scope', function($scope) {
     console.log($scope.message);
   };
 
-  $scope.save = function() {
-    var uuid = __guid();
-
-    var a = btoa(JSON.stringify($scope.states));
-    console.log(a);
-    console.log(JSON.parse(atob(a)));
-
-    console.log(uuid);
-  };
-
   var __readSequence = function(index, state) {
-
     var returnState = state;
 
     if (state.direction != "stop") {
@@ -94,7 +82,6 @@ builder.controller("machine.main", ['$scope', function($scope) {
           break;
         }
       }
-
     }
 
     return returnState;
@@ -103,20 +90,5 @@ builder.controller("machine.main", ['$scope', function($scope) {
   var __setCharAt = function(str, index, character) {
     return str.substr(0, index) + character + str.substr(index + character.length)
   };
-
-  /** From StackOverflow:
-      http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/105074#105074
-  */
-  var __guid = (function() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-                 .toString(16)
-                 .substring(1);
-    }
-    return function() {
-      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-             s4() + '-' + s4() + s4() + s4();
-    };
-  })();
 
 }]);
