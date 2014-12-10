b2b08d074ad28cbdf5004f6f2194019fce6ad76f
Prateem Shrestha <prateems@gmail.com>
2014-12-10 03:05:22 -0500

    Clean up mostly completed. Seems ready for use.

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 
 var builder = angular.module("machine", []);
 
-builder.controller("machine.main", ['$scope', function($scope) {
+builder.controller("machine.main", ['$scope', function ($scope) {
 
   $scope.states = null;
   $scope.sequence = "";
   $scope.result = "";
   $scope.runLog = [];
   $scope.finalState = null;
+  $scope.message = null;
 
   $scope.newState = {
     direction: 'right'
@@ -20,15 +21,12 @@ builder.controller("machine.main", ['$scope', function($scope) {
     'stop': 'Stop'
   };
 
-  $scope.reset = function() {
-    $scope.newState = {
-      direction: 'right'
-    };
-
+  $scope.startOver = function() {
+    __reset();
     $scope.states = null;
-  };
+  }
 
-  $scope.addState = function() {
+  $scope.addState = function () {
     if ($scope.states == null) {
       $scope.states = [];
     }
@@ -40,37 +38,60 @@ builder.controller("machine.main", ['$scope', function($scope) {
     };
   };
 
-  $scope.run = function() {
-    var __machineRunCompleted = false;
-    var initialChar = $scope.sequence.charAt(0);
+  $scope.run = function () {
+    __reset();
+    $scope.machineRunCompleted = false;
 
-    for (var state of $scope.states) {
-      if (state.name == "initial" && state.reads == initialChar) {
-        $scope.result = $scope.sequence;
+    if ($scope.states != null) {
+      var initialChar = $scope.sequence.charAt(0);
 
-        $scope.finalState = (state.direction != "stop")
-            ? __readSequence(0, state) : state;
+      for (var state of $scope.states) {
+        if (state.name == "initial" && state.reads == initialChar) {
+          $scope.result = $scope.sequence;
 
-        __machineRunCompleted = true;
-        break;
+          $scope.finalState = __readSequence(0, state);
+
+          $scope.machineRunCompleted = true;
+          break;
+        }
       }
-    }
 
-    if (!__machineRunCompleted) {
-      $scope.message = "No initial state that begins reading the given input sequence was defined.";
+      if (!$scope.machineRunCompleted) {
+        __setMessage("error", "No initial state that begins reading the given input sequence was defined.");
+      } else {
+        __setMessage("success", "The machine has successfully started and reached a stop point.");
+      }
     } else {
-      $scope.message = finalState;
+      __setMessage("error", "There are no states on the machine.");
     }
 
-    console.log($scope.message);
+    $scope.runLog.reverse();
+    $scope.original = angular.copy($scope.sequence);
   };
 
-  var __readSequence = function(index, state) {
+  var __setMessage = function(type, text) {
+    $scope.message = {
+      type: type,
+      text: text
+    };
+  };
+
+  var __reset = function () {
+    $scope.newState = {
+      direction: 'right'
+    };
+
+    $scope.runLog = [];
+    $scope.message = null;
+  };
+
+  var __readSequence = function (index, state) {
     var returnState = state;
+    var toLog = "State '" + state.name + "' read '" + state.reads + "'.";
 
     if (state.direction != "stop") {
-      
       $scope.result = __setCharAt($scope.result, index, state.writes);
+      toLog += " Replaced with '" + state.writes + "'.";
 
       index = index + (state.direction == 'left' ? -1 : 1);
       var nextChar = (index < 0 || index > ($scope.sequence.length - 1))
@@ -78,16 +99,20 @@ builder.controller("machine.main", ['$scope', function($scope) {
 
       for (var newState of $scope.states) {
         if (newState.name == state.becomes && newState.reads == nextChar) {
+          toLog += " Moved to state '" + newState.name + "'.";
           returnState = __readSequence(index, newState);
           break;
         }
       }
+    } else {
+      toLog += " Next state: '" + state.becomes + "'. Stopping.";
     }
 
+    $scope.runLog.push(toLog);
     return returnState;
   };
 
-  var __setCharAt = function(str, index, character) {
+  var __setCharAt = function (str, index, character) {
     return str.substr(0, index) + character + str.substr(index + character.length)
   };
 
